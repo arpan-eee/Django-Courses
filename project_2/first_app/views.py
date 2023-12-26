aebc665dca15bfef2608857679595e0fa9184390
Arpan Chakraborty <arpan.chakraborty9876@gmail.com>
2023-12-26 13:56:56 +0600

    second demo project

---
+++ project_2/first_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    # return HttpResponse("hi ")
+    return render(request,"first_app/home.html")
+
