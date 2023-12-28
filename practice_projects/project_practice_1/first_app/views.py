a5410dab964d6a12549ff86c7c456128d321c5ad
Arpan Chakraborty <arpan.chakraborty9876@gmail.com>
2023-12-28 14:54:32 +0600

    practice 10.5 done

--- practice_projects/project_practice_1/first_app/views.py
+++ practice_projects/project_practice_1/first_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'first_app/home.html')
+   
+    d={'Name':"Arpan",'age':22 ,'slash':"I'm Arpan" ,'datetime':datetime.datetime.now(),'listDic':[{'name': 'zed', 'age': 19},{'name': 'amy', 'age': 22},{'name': 'joe', 'age': 31},] ,'multiline':"""one
+       two
+       three""",'string':"my first project"}
+    return render(request, 'first_app/home.html',d)
