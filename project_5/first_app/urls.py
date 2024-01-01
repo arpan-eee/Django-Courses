dbb7ca621ff1ae1089180309fc16db934cc84d4d
Arpan Chakraborty <arpan.chakraborty9876@gmail.com>
2024-01-01 22:15:09 +0600

    Form API

--- project_5/first_app/urls.py
+++ project_5/first_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
-    path('form/', views.submit_form, name='form'),
-    path('django_form/', views.DjangoForm, name='django_form'),
-    path('about/', views.about, name='about'),
+    path('', views.home, name = "homepage"),
+    path('about/', views.about, name = "aboutpage"),
+    path('form/', views.submit_form, name = "submit_form"),
+    path('django_form/', views.PasswordValidation, name = "django_form"),
 ]
--- project_5/first_app/views.py
+++ project_5/first_app/views.py
@@ -1,30 +1,52 @@
 from django.shortcuts import render
-from . forms import contactForm
-
+from . forms import contactForm, StudentData, PasswordValidationProject
 # Create your views here.
-
+def home(request):
+    return render(request, './first_app/home.html')
+    
 def about(request):
     if request.method == 'POST':
-        # print(request.POST)
+        print(request.POST)
         name = request.POST.get('username')
         email = request.POST.get('email')
         select = request.POST.get('select')
-        return render(request,"about.html",{'name':name,'email':email,'select':select})
-    return render(request,"about.html")
+        return render(request, './first_app/about.html', {'name' : name, 'email': email, 'select' : select})
+    else:
+        return render(request, './first_app/about.html')
+        
 
 def submit_form(request):
-    return render(request,"form.html")
+    return render(request, './first_app/form.html')
 
 def DjangoForm(request):
     if request.method == 'POST':
-        form = contactForm(request.POST,request.FILES)
+        form = contactForm(request.POST, request.FILES)
         if form.is_valid():
-            # file = form.cleaned_data['file'] 
-            # with open('./first_app/upload/' + file.name, 'wb+') as destination :
-            #     for chunk in file.chunks() : 
+            # file = form.cleaned_data['file']
+            # with open('./first_app/upload/' + file.name, 'wb+') as destination:
+            #     for chunk in file.chunks():
             #         destination.write(chunk)
             print(form.cleaned_data)
-        return render(request,"django_form.html",{'form':form})
     else:
         form = contactForm()
-        return render(request,"django_form.html")
+    return render(request, './first_app/django_form.html', {'form':form}) 
+   
+   
+def StudentForm(request):
+    if request.method == 'POST':
+        form = StudentData(request.POST, request.FILES)
+        if form.is_valid():
+            print(form.cleaned_data)
+    else:
+        form = StudentData()
+    return render(request, './first_app/django_form.html', {'form':form})  
+  
+def PasswordValidation(request):
+    if request.method == 'POST':
+        form = PasswordValidationProject(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+    else:
+        form = PasswordValidationProject()
+    return render(request, './first_app/django_form.html', {'form':form})    
+
