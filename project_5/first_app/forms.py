05996e1124a79b70125cad6c1fc33dae7df9a43a
Arpan Chakraborty <arpan.chakraborty9876@gmail.com>
2024-01-01 20:00:53 +0600

    Django Form Attributes & Widgets

--- project_5/first_app/forms.py
+++ project_5/first_app/forms.py
@@ -1,5 +1,16 @@
 from django import forms
 
 class contactForm(forms.Form):
-    name = forms.CharField(label="User Name")
-    email = forms.EmailField(label="User Email")
+    name = forms.CharField(label="Full Name : ", help_text="Total length must be within 70 characters", required=False, error_messages={'required': 'Please enter your name.'},widget = forms.Textarea(attrs = {'id' : 'text_area', 'class' : 'class1 class 2', 'placeholder' : 'Enter your name'},))
+    email = forms.EmailField(label = "User Email")
+    # age = forms.IntegerField()
+    # weight = forms.FloatField()
+    # balance = forms.DecimalField()
+    age = forms.CharField(widget=forms.NumberInput)
+    check = forms.BooleanField()
+    birthday = forms.CharField(widget=forms.DateInput(attrs= {'type' : 'date'}))
+    appointment = forms.CharField(widget=forms.DateInput(attrs= {'type' : 'datetime-local'}))
+    CHOICES = [('S', 'Small'), ('M', 'Medium'), ('L', 'Large')]
+    size = forms.ChoiceField(choices=CHOICES, widget = forms.RadioSelect)
+    MEAL = [('P', 'Pepperoni'), ('M', 'Mashroom'), ('B', 'Beef')]
+    pizza = forms.MultipleChoiceField(choices=MEAL, widget=forms.CheckboxSelectMultiple)
--- project_5/first_app/views.py
+++ project_5/first_app/views.py
@@ -16,7 +16,15 @@ def submit_form(request):
     return render(request,"form.html")
 
 def DjangoForm(request):
-    form = contactForm(request.POST)
-    if form.is_valid():
-        print(form.cleaned_data)
-    return render(request,"django_form.html",{'form':form})
+    if request.method == 'POST':
+        form = contactForm(request.POST,request.FILES)
+        if form.is_valid():
+            # file = form.cleaned_data['file'] 
+            # with open('./first_app/upload/' + file.name, 'wb+') as destination :
+            #     for chunk in file.chunks() : 
+            #         destination.write(chunk)
+            print(form.cleaned_data)
+        return render(request,"django_form.html",{'form':form})
+    else:
+        form = contactForm()
+        return render(request,"django_form.html")
