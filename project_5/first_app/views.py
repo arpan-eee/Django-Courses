33793e58361b82262525ea67513eec078b646781
Arpan Chakraborty <arpan.chakraborty9876@gmail.com>
2024-01-01 14:06:41 +0600

    more html form

--- project_5/first_app/views.py
+++ project_5/first_app/views.py
@@ -4,9 +4,11 @@ from django.shortcuts import render
 
 def about(request):
     if request.method == 'POST':
+        # print(request.POST)
         name = request.POST.get('username')
         email = request.POST.get('email')
-        return render(request,"about.html",{'name':name,'email':email})
+        select = request.POST.get('select')
+        return render(request,"about.html",{'name':name,'email':email,'select':select})
     return render(request,"about.html")
 
 def submit_form(request):
