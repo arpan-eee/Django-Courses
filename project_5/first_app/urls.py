479c3dc6efd369296b9c765f61a19db427b20c48
Arpan Chakraborty <arpan.chakraborty9876@gmail.com>
2024-01-01 15:16:16 +0600

    built in forms django

--- project_5/first_app/urls.py
+++ project_5/first_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('form/', views.submit_form, name='form'),
+    path('django_form/', views.DjangoForm, name='django_form'),
     path('about/', views.about, name='about'),
 ]
--- project_5/first_app/views.py
+++ project_5/first_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from . forms import contactForm
 
 # Create your views here.
 
@@ -12,8 +13,10 @@ def about(request):
     return render(request,"about.html")
 
 def submit_form(request):
-    if request.method == 'POST':
-        name = request.POST.get('username')
-        email = request.POST.get('email')
-        return render(request,"form.html",{'name':name,'email':email})
     return render(request,"form.html")
+
+def DjangoForm(request):
+    form = contactForm(request.POST)
+    if form.is_valid():
+        print(form.cleaned_data)
+    return render(request,"django_form.html",{'form':form})
