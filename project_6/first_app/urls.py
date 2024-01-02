cb4a39a7c9fe53d92aca5732bb4bcecaf5bc5243
Arpan Chakraborty <arpan.chakraborty9876@gmail.com>
2024-01-02 13:08:40 +0600

    delete from frontend

--- project_6/first_app/urls.py
+++ project_6/first_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home  ),
+    path('', views.home,name="home"  ),
+    path('delete/<int:roll>', views.delete_student ,name="delete" ),
 ]
--- project_6/first_app/views.py
+++ project_6/first_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from . import models
 
 # Create your views here.
@@ -6,3 +6,7 @@ from . import models
 def home(request):
     students = models.students.objects.all()
     return render(request, 'home.html',{'students':students})
+
+def delete_student(request,roll):
+    std = models.students.objects.get( pk = roll ).delete()
+    return redirect("home")
