0b45a46029b719b1dbae9f872a214a8a97d90d98
Arpan Chakraborty <arpan.chakraborty9876@gmail.com>
2024-01-03 22:53:39 +0600

    Task Management Basic Setup

---
+++ Assignments/Task_Management_System/category/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def add_category(request):
+    return render (request ,'add_category.html')
---
+++ Assignments/Task_Management_System/task/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def add_task(request):
+    return render (request ,'add_task.html')
