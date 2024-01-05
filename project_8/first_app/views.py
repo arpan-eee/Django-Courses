136ba13f4d6577ba2b85cc3ee0a0ee6e8c53ba45
Arpan Chakraborty <arpan.chakraborty9876@gmail.com>
2024-01-05 21:48:12 +0600

    change user data

--- project_8/first_app/views.py
+++ project_8/first_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from .forms import RegisterForm
+from .forms import RegisterForm,ChangeUserData
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm,PasswordChangeForm,SetPasswordForm
 from django.contrib.auth import authenticate,login,logout,update_session_auth_hash
@@ -42,9 +42,17 @@ def user_login(request):
     
 def profile(request):
     if request.user.is_authenticated:
-        return render(request, 'profile.html',{'user':request.user})
+        if request.method == 'POST':
+            form = ChangeUserData(request.POST,instance=request.user)
+            if form.is_valid():
+                messages.success(request,'Account updated successfully')
+                form.save()
+                print (form.cleaned_data)
+        else:
+            form = ChangeUserData(instance=request.user)
+        return render(request, 'profile.html',{'form':form})
     else:
-        return redirect('login')
+        return redirect('signup')
 
 def user_logout(request):
     logout(request)
@@ -77,3 +85,17 @@ def pass_change2(request):
         return render(request,'passchange.html',{'form':form})
     else:
         return redirect('login')
+    
+def change_user_data(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = ChangeUserData(request.POST,instance=request.user)
+            if form.is_valid():
+                messages.success(request,'Account updated successfully')
+                form.save()
+                print (form.cleaned_data)
+        else:
+            form = ChangeUserData()
+        return render(request, 'profile.html',{'form':form})
+    else:
+        return redirect('signup')
