adcd61434065463b2ea8d653823caa90aef3418b
Arpan Chakraborty <arpan.chakraborty9876@gmail.com>
2024-01-07 11:33:46 +0600

    slug field

--- Blog Project Part 2/blog_project/categories/admin.py
+++ Blog Project Part 2/blog_project/categories/admin.py
@@ -3,4 +3,10 @@ from . import models
 
 # Register your models here.
 
-admin.site.register(models.Category)
+# admin.site.register(models.Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+    list_display = ['name','slug']
+
+admin.site.register(models.Category,CategoryAdmin)
--- Blog Project Part 2/blog_project/categories/models.py
+++ Blog Project Part 2/blog_project/categories/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100,unique=True,null=True,blank=True)
 
     def __str__(self):
         return f"{self.name}"
